14cda57624e2bf3d2ec39f6ea8c8897590ac8223
Yawar <yawarsdq@gmail.com>
2025-10-06 15:27:33 +0530

    Package Name Change

--- src/test/java/com/SerializationDeserialization/Runner.java
+++ src/test/java/com/SerializationDeserialization/Runner.java
@@ -1,4 +1,4 @@
-package com.demo;
+package com.SerializationDeserialization;
 
 import com.google.gson.Gson;
 
--- src/test/java/com/SerializationDeserialization/RunnerByJacksonConversion.java
+++ src/test/java/com/SerializationDeserialization/RunnerByJacksonConversion.java
@@ -1,6 +1,5 @@
-package com.demo;
+package com.SerializationDeserialization;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RunnerByJacksonConversion {
